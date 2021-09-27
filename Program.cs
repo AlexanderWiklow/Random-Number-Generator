4a414bf9044eba0f55d17c983b6b141032fb2405
Alexander <a.wiklow@gmail.com>
2021-09-27 17:27:07 +0200

    create a number generator

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Det_hemliga_talet__del_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // En kod som slumpar fram ett tal mellan ett och hundra som sedan skrivs ut
+            Random randomerare = new Random();
+            int slump_tal = randomerare.Next(1, 101);
+
+            // Skriver ut talet till användaren
+            Console.WriteLine(slump_tal);
+        }
+    }
+}
